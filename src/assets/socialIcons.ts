da8d1c7abb637ecd0bb73ac77bd0e1ee5cb7b1bb
AbdulHaseebKashmirii <k225069@nu.edu.pk>
2025-01-02 11:45:23 +0500

    comment

--- src/assets/socialIcons.ts
+++ src/assets/socialIcons.ts
@@ -2,12 +2,13 @@ const socialIcons = {
   Kaggle: `<svg 
   xmlns="http://www.w3.org/2000/svg" 
   viewBox="0 0 24 24" 
-  class="icon-tabler">
-  <path d="M0 0h24v24H0z" fill="none" />
-  <path d="M10.5 8v8" />
-  <path d="M14.5 8l-3 4l3 4" />
-  <path d="M10.5 12h1" />
-  </svg>`,
+  fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+  class="icon-tabler icon-tabler-kaggle">
+  <rect x="2" y="2" width="20" height="20" rx="5" />
+  <path d="M10 9v6" />
+  <path d="M14 9l-4 3 4 3" />
+</svg>
+`,
   Github: `<svg
     xmlns="http://www.w3.org/2000/svg"
     class="icon-tabler"
